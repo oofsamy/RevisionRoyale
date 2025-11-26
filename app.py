2da67a1e20779945d527025d1983ac3999e5af07
oofsamy <teamssglol@gmail.com>
2025-11-26 01:23:50 +0000

    Added the core functionality for the startup page
    Added HTML functionality for the startup page
    Added CSS design for the startup page (not yet finished)
    Removed index.html as it's redundant
    Began working on main.html

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('startup.html')
 
 @app.route('/login')
 def login():
+    print(request.args)
+
     return render_template('login.html')
 
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/startup')
+def startup():
+    return render_template('startup.html')
+
+@app.route('/main')
+def mb_test():
+    return render_template("main.html", Authenticated=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
