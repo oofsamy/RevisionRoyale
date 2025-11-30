bcf9ad284e18732e14115d4024400d6ed566cf40
oofsamy <teamssglol@gmail.com>
2025-11-30 19:12:58 +0000

    Added error page and allowed external hosting

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ def index():
 
 @app.route('/login')
 def login():
-    print(request.args)
-
     return render_template('login.html')
 
 @app.route('/register')
@@ -22,7 +20,11 @@ def startup():
 
 @app.route('/main')
 def mb_test():
-    return render_template("main.html", Authenticated=True)
+    return render_template("main.html", Authenticated=True, Text=request.remote_addr)
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template("error.html", status_code=404, error_message="Page not found.")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
