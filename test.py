437d374bdf2597ce958a06a94ca6718bcfa4f8a6
oofsamy <teamssglol@gmail.com>
2026-01-19 00:45:37 +0000

    Removed ALL redundant code, added AutoIncrement to tables that need it, GetRecord fixed
    Huge commit

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ class Table:
         pass
 
 class Record:
-    def __init__(self, TableName: str = None, PrimaryKey: AttributeValue = None, Attributes: list[AttributeValue] = None): #will have a primary key attribute I believe, table attribute, 
+    def __init__(self, TableName: str = None, PrimaryKey: AttributeValue = None, Attributes: list[AttributeValue] = None):
         self.TableName = TableName
         self.PrimaryKey = PrimaryKey
         self.Attributes = Attributes
@@ -39,11 +39,12 @@ class Record:
     def SetPrimaryKey(self, PrimaryKey: AttributeValue) -> None:
         self.PrimaryKey = PrimaryKey
 
-    def AddAttribute(self, Attribute: AttributeValue):
+    def AddAttribute(self, Attribute: AttributeValue) -> None:
         self.Attributes.append(Attribute)
 
-class User(Record): # maybe have a hierarchy chart (maybe even add this post design??) of how everything inherits this base record class to make it serialisable?
-    pass
+    def IsEmpty(self) -> bool:
+        return (self.TableName == "" or self.PrimaryKey == None)
+        ## A valid record will have a Table and a PrimaryKey, if not, it is not valid 
 
 class Database:
     def __init__(self, FileName: str) -> None: ## Constructor method of Database class
@@ -64,10 +65,10 @@ class Database:
         ### Friends table is generated in case it doesn't already exist
         self.GenerateTable(TableName="Friends", PrimaryKey=Attribute("FriendID", "INTEGER"), AutoIncrementPrimaryKey=True,
                             Attributes=[Attribute("EstablishedDate", "INTEGER")],
-                            ForeignKeyAttributes=[ForeignKeyAttribute("ReceiverUsername", "TEXT", "Users", "Username")]) #mention that you changed ReceiverUserID to ReceiverUsername
+                            ForeignKeyAttributes=[ForeignKeyAttribute("ReceiverUsername", "TEXT", "Users", "Username")])
         
         ### Subjects table is generated in case it doesn't already exist
-        self.GenerateTable(TableName="Subjects", PrimaryKey=Attribute("SubjectName", "TEXT"),
+        self.GenerateTable(TableName="Subjects", PrimaryKey=Attribute("SubjectName", "TEXT"), 
                            Attributes=[Attribute("ExamBoard", "TEXT"),
                                        Attribute("TimeSpent", "INTEGER"),
                                        Attribute("LastReviewed", "INTEGER"),
@@ -75,13 +76,13 @@ class Database:
         
         ### Decks table is generated in case it doesn't already exist
         #mention that upon utilising sqlite, number is not a valid data type
-        self.GenerateTable(TableName="Decks", PrimaryKey=Attribute("DeckID", "INTEGER"), 
+        self.GenerateTable(TableName="Decks", PrimaryKey=Attribute("DeckID", "INTEGER"), AutoIncrementPrimaryKey=True,
                            Attributes=[Attribute("DeckName", "TEXT")],
                            ForeignKeyAttributes=[ForeignKeyAttribute("Username", "TEXT", "Users", "Username"),
-                                                 ForeignKeyAttribute("Subject", "TEXT", "Subjects", "SubjectName")]) ### also had to change from userid to username here, and subjectID foreign key wasn't right in the NEA design, from SubjectID to just Subject..?
+                                                 ForeignKeyAttribute("Subject", "TEXT", "Subjects", "SubjectName")])
 
         ### Flashcards table is generated in case it doesn't already exist
-        self.GenerateTable(TableName="Flashcards", PrimaryKey=Attribute("FlashcardID", "INTEGER"),
+        self.GenerateTable(TableName="Flashcards", PrimaryKey=Attribute("FlashcardID", "INTEGER"), AutoIncrementPrimaryKey=True,
                            Attributes=[Attribute("FrontContent", "TEXT"),
                                        Attribute("BackContent", "TEXT"),
                                        Attribute("LastReviewed", "INTEGER"),
@@ -90,86 +91,32 @@ class Database:
                                        Attribute("NextDue", "INTEGER"),
                                        Attribute("Difficulty", "REAL"),
                                        Attribute("Stability", "REAL")],
-                            ForeignKeyAttributes=[ForeignKeyAttribute("Username", "TEXT", "Users", "Username"), ## changed to username here too
+                            ForeignKeyAttributes=[ForeignKeyAttribute("Username", "TEXT", "Users", "Username"),
                                                   ForeignKeyAttribute("DeckID", "INTEGER", "Decks", "DeckID")]) 
         
         ### RevisionSessions table is generated in case it doesn't already exist
-        self.GenerateTable(TableName="RevisionSessions", PrimaryKey=Attribute("SessionID", "INTEGER"),
+        self.GenerateTable(TableName="RevisionSessions", PrimaryKey=Attribute("SessionID", "INTEGER"), AutoIncrementPrimaryKey=True,
                             Attributes=[Attribute("StartTime", "INTEGER"),
                                         Attribute("Duration", "INTEGER"),
                                         Attribute("CardsReviewed", "INTEGER")],
-                            ForeignKeyAttributes=[ForeignKeyAttribute("Username", "TEXT", "Users", "Username"), ## changed to username here too
+                            ForeignKeyAttributes=[ForeignKeyAttribute("Username", "TEXT", "Users", "Username"),
                                                   ForeignKeyAttribute("DeckID", "INTEGER", "Decks", "DeckID"),
                                                   ForeignKeyAttribute("Subject", "TEXT", "Subjects", "SubjectName")])
         
         ### TimetableSlots table is generated in case it doesn't already exist
-        self.GenerateTable(TableName="TimetableSlots", PrimaryKey=Attribute("TimetableSlotID", "INTEGER"),
+        self.GenerateTable(TableName="TimetableSlots", PrimaryKey=Attribute("TimetableSlotID", "INTEGER"), AutoIncrementPrimaryKey=True,
                            Attributes=[Attribute("PlannedDuration", "INTEGER"),
                                        Attribute("DayOfWeek", "TEXT")],
                            ForeignKeyAttributes=[ForeignKeyAttribute("DeckID", "INTEGER", "Decks", "DeckID")])
 
         ### Timetable table is generated in case it doesn't already exist
-        self.GenerateTable(TableName="Timetable", PrimaryKey=Attribute("TimetableID", "INTEGER"),
+        self.GenerateTable(TableName="Timetable", PrimaryKey=Attribute("TimetableID", "INTEGER"), AutoIncrementPrimaryKey=True,
                            ForeignKeyAttributes=[ForeignKeyAttribute("TimetableSlotID", "INTEGER", "TimetableSlots", "TimetableSlotID")])
 
 
-        #self.CreateRecord("Users", PrimaryKey=AttributeValue("Username", Type=None, Value="oofsamy"), Attributes=[AttributeValue("HashedPassword", Type=None, Value="ExampleHash"),
-                                                                                                                  # AttributeValue("Level", Type=None, Value=2),
-                                                                                                                  # AttributeValue("LastActive", Type=None, Value=time.time()),
-                                                                                                                  # AttributeValue("JoinDate", Type=None, Value=time.time()),
-                                                                                                                  # AttributeValue("SetupComplete", Type=None, Value=1),
-                                                                                                                  # AttributeValue("Streak", Type=None, Value=1)])
-
-        #MyUser: Record = self.GetRecord(TableName = "Users", Attribute=AttributeValue(Name="Username", Type=None, Value="oofsamy"))
-        #MyUser: Record = self.GetRecord(TableName = "", Attribute=AttributeValue(Name = "", Type = "", Value = None))
-        #MyUser: Record = self.GetRecord(TableName = "FooBar", Attribute=AttributeValue(Name = "Username", Type = "", Value = "test"))
-        MyUser: Record = self.GetRecord(TableName = "Decks", Attribute=AttributeValue(Name = "DeckID", Type = None, Value = "Test"))
-
-        
-
-        print(MyUser.TableName, MyUser.PrimaryKey, MyUser.Attributes)
-
-        # self.Cursor.execute("SELECT * FROM Users;")
-        # print(self.Cursor.fetchall())
-
-        #self.GetRecord(TableName="Friends", Attribute="ReceiverUsername", Value="oofsamy")
-
-        #for x in range(21):
-        #    print(self.CreateRecord("Friends", AutoIncrementPrimaryKey = True, Attributes=[AttributeValue(Name = "ReceiverUsername", Type=None, Value="spidenix")]))
-
-        #print(self.CreateRecord("Users", PrimaryKey=AttributeValue("Username", Type=None, Value="oofsamy"), Attributes=[AttributeValue("Level", Type=None, Value=4)]))
-        #print(self.CreateRecord("Friends", PrimaryKey=AttributeValue("FriendID", Type=None, Value=4), Attributes=[AttributeValue("ReceiverUsername", Type=None, Value="oofsamy")]))
-        #print(self.CreateRecord("Friends", PrimaryKey=AttributeValue("FriendID", Type=None, Value=5), Attributes=[AttributeValue("ReceiverUsername", Type=None, Value="asdasd")]))
-        #print(self.CreateRecord(TableName="FooBar", PrimaryKey=AttributeValue("ID", Type=None, Value="Test")))
-        #print(self.CreateRecord(TableName="Users", Attributes=[AttributeValue("Level", Type=None, Value=4)]))
-
-        # self.Connection.close() I don't think you should call this until the end of the database usage.
-
     ### Generates a table within the chosen database file
-    ### Required arguments are: TableName and PrimaryKey
-    ### Optional arguments are: The list of normal attributes and the list of foreign key attributes
-
-    ### maybe think abt autoincrement primary key, requiring a new parameter, will make primarykey an optional parameter for CreateRecord
-    def GenerateTableOld(self, TableName: str, PrimaryKey: Attribute, Attributes: list[Attribute] = None, ForeignKeyAttributes: list[ForeignKeyAttribute] = None) -> None:
-        RecordDefinitions: list[str] = [f"{PrimaryKey.Name} {PrimaryKey.Type} PRIMARY KEY"]
-
-        if Attributes != None:
-            for Attribute in Attributes: ## Defines normal attributes columns 
-                RecordDefinitions.append(f"{Attribute.Name} {Attribute.Type}")
-
-        if ForeignKeyAttributes != None:
-            for ForeignAttribute in ForeignKeyAttributes: ## Defines foreign key attributes columns
-                RecordDefinitions.append(f"{ForeignAttribute.Name} {ForeignAttribute.Type}")
-
-            for ForeignAttribute in ForeignKeyAttributes: ## Defines foreign key constraints
-                RecordDefinitions.append(f"FOREIGN KEY({ForeignAttribute.Name}) REFERENCES {ForeignAttribute.ForeignTable}({ForeignAttribute.ForeignName})")
-
-        RecordString: str = ",\n".join(RecordDefinitions)
-        CommandString: str = f"CREATE TABLE IF NOT EXISTS {TableName}\n({RecordString})" ## Required in case table already exists
-
-        self.Cursor.execute(CommandString)
-        self.Connection.commit()
-
+    ### Required arguments are: TableName and Primary Key
+    ### Optional arguments are: AutoIncrementPrimaryKey, Attributes, ForeignKeyAttributes 
     def GenerateTable(self, TableName: str, PrimaryKey: Attribute, AutoIncrementPrimaryKey: bool = False, Attributes: list[Attribute] = None, ForeignKeyAttributes: list[ForeignKeyAttribute] = None) -> None:
         RecordDefinitions: list[str] = []
 
@@ -199,30 +146,7 @@ class Database:
         os.makedirs("./Data/", exist_ok=True) # Makes the folder containing the database file
         if os.path.exists(FileName) == False: # Checks if the database file doesn't exist before creating a new one
             DatabaseFile = open(FileName, 'w')
-
-    def CreateRecordOld(self, TableName: str, PrimaryKey: AttributeValue, Attributes: list[AttributeValue] = None, ForeignKeyAttributes: list[ForeignKeyAttributeValue] = None) -> None:
-        CommandString = f"INSERT INTO {TableName} (" ## Required for the SQL statement
-        AttributeNames = [PrimaryKey.Name] #Begins the list of attributes' names
-        AttributeValues = [f"'{str(PrimaryKey.Value)}'"] #Begins the list of attributes' values
-
-        for Attribute in Attributes:
-            AttributeNames.append(Attribute.Name) # Adds name of the attribute to the list of names
-            AttributeValues.append(f"'{str(Attribute.Value)}'") # Adds the respective value of the attribute to the list of values
-
-        AttributeNamesString = ", ".join(AttributeNames) #Converts the array into a string
-        AttributeValuesString = ", ".join(AttributeValues) #Converts the array into a string
-
-        CommandString = CommandString + AttributeNamesString + ') VALUES\n(' + AttributeValuesString + ');' # Combines all strings
-
-        self.Cursor.execute(CommandString) ## Commits the change to persistent storage
-        self.Connection.commit()
         
-        ## do a test where empty primary key
-        ## do a test where table doesn't exist
-        ## create a test error where an already existign non unique pk entree is made
-        ## maybe write about foreignkeyattributes being an unused variable and so its redundant in the remedial dev
-        ## do a foreign key test
-
     def CreateRecord(self, TableName: str, PrimaryKey: AttributeValue = None, AutoIncrementPrimaryKey: bool = False, Attributes: list[AttributeValue] = None) -> str: ## AutoIncrement parameter boolean, you also made PrimaryKey an optional thing since it is now
         CommandString = f"INSERT INTO {TableName} (" ## Required for the SQL statement
         AttributeNames = []
@@ -252,14 +176,10 @@ class Database:
         else:
             return "Successfully created record into database."
 
-    ## remember to remedial develop another stage for the GenerateTable method to return errors instead!!!
-
     def GetRecord(self, TableName: str, Attribute: AttributeValue) -> Record:
         if TableName == "" or Attribute.Name == "" or Attribute.Value == "":
             return Record()
         else:
-            ReturnRecord = Record()
-
             NamesString = f"PRAGMA table_info({TableName})"
             self.Cursor.execute(NamesString)
 
@@ -278,10 +198,16 @@ class Database:
 
             ## Grabs the columns values, as before we only get the attributes names and types, but no values tied.
 
-            SelectString = f"SELECT * FROM {TableName} WHERE {Attribute.Name} = \"{Attribute.Value}\""
-            self.Cursor.execute(SelectString)
+            try:
+                SelectString = f"SELECT * FROM {TableName} WHERE {Attribute.Name} = \"{Attribute.Value}\""
+                self.Cursor.execute(SelectString)
+            except sqlite3.OperationalError as Error:
+                return Record()
 
             Output = self.Cursor.fetchone()
+
+            if Output == None: ## Null-checking
+                return Record()
 
             for Index in range(len(Output)):
                 if Index == PrimaryKeyIndex:
@@ -293,22 +219,7 @@ class Database:
 
             return Record(TableName = TableName, PrimaryKey = PrimaryKey, Attributes=Attributes)
 
-    # def GetRecord(self, TableName, Attribute, Value) -> Record: ## boy, talk about how when testing this function, u realised the database aint saving like at all man, and u only realised via this function.
-    #         print("Cannot have empty arguments")
-
-    #         return Record()S
-
-    #     SelectString = f"SELECT * FROM {TableName} WHERE {Attribute}={Value}"
-
-    #     self.Cursor.execute(SelectString)
-        
-    #     Output = self.Cursor.fetchone()
-
-    #     print(Output)
-
-        ## SELECT * FROM TableName WHERE
-
-    def __del__(self): ## talk about destructor method being added here
+    def __del__(self):
         self.Connection.close()
 
 class AuthenticationModule:
@@ -328,24 +239,14 @@ class AuthenticationModule:
     def Register(Username: str, ) -> bool:
         pass
 
-# class User:
-#     def __init__(self, Username, HashedPassword): #wait rethink this, is this gonna be used when the user registers or just when any user object is made?
-#         self.UserID = #GeneratePrimaryKey() #idrk how this works in python sql 
-#         self.Username = Username 
-#         self.HashedPassword = HashedPassword
-#         self.Level = CONSTANTS.DEFAULT_LEVEL
-#         self.LastActive = Timestamp()
-#         self.JoinDate = Timestamp()
-#         self.SetupComplete = # 
-#         self.Streak = #
-#         self.HideActivity = #
+class Authentication:
+    pass
 
-ProgramDatabase: Database = None #PDatbase: Database typing
+
+ProgramDatabase: Database = None
 
 def Main():
     ProgramDatabase = Database(CONSTANTS.DEFAULT_DATABASE_LOCATION)
 
-#This line of code below checks to see if the python script is being run directly (rather than a module)
- # first time running app checks should be made here, such as checking if database.db file exists and blah..
 if __name__ == "__main__":
     Main()
